ce923542ee531904fc783b874bf57d09c886f403
Thanh Doan <doan.teddy@gmail.com>
2023-08-04 13:46:47 -0400

    added routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
-const courseRoutes = require('./courseRoutes');
-const studentRoutes = require('./studentRoutes');
+const usersRoutes = require('./usersRoutes');
+const thoughtsRoutes = require('./thoughtsRoutes');
 
-router.use('/courses', courseRoutes);
-router.use('/students', studentRoutes);
+router.use('/user', userRoutes);
+router.use('/thought', thoughtRoutes);
 
 module.exports = router;
