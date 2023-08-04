04683be51b21603f775c9d96b9d1e45e824871ce
Thanh Doan <doan.teddy@gmail.com>
2023-08-04 12:54:56 -0400

    copied file structure from Mini Project

---
+++ controllers/thoughtController.js
@@ -0,0 +1,77 @@
+const { User, Thought  } = require('../models');
+
+module.exports = {
+  // Get all courses
+  async getThoughts(req, res) {
+    try {
+      const thoughts = await Thought.find();
+      res.json(thoughts);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  // Get a course
+  async getSingleThought(req, res) {
+    try {
+      const thought = await Thought.findOne({ _id: req.params.thoughtId })
+        .select('-__v');
+
+      if (!thought) {
+        return res.status(404).json({ message: 'No Thought with that ID' });
+      }
+
+      res.json(thought);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  // Create a course
+  async createThought(req, res) {
+    try {
+      const thought = await Thought.create(req.body);
+      const user = await User.findOneAndUpdate(
+        { _id: req.body.userId },
+        // same as a push
+        { $push: { thoughts: thought._id } },
+        { new: true }
+      );
+      res.json(thought);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
+  // Delete a course
+  async deleteCourse(req, res) {
+    try {
+      const course = await Course.findOneAndDelete({ _id: req.params.courseId });
+
+      if (!course) {
+        res.status(404).json({ message: 'No course with that ID' });
+      }
+
+      await Student.deleteMany({ _id: { $in: course.students } });
+      res.json({ message: 'Course and students deleted!' });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  // Update a course
+  async updateCourse(req, res) {
+    try {
+      const course = await Course.findOneAndUpdate(
+        { _id: req.params.courseId },
+        { $set: req.body },
+        { runValidators: true, new: true }
+      );
+
+      if (!course) {
+        res.status(404).json({ message: 'No course with this id!' });
+      }
+
+      res.json(course);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+};
